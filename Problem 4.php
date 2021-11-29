7a44c28f7ea6a37da368f1039be06642cf63170c
WASIF HUSSAIN <hussainwasif33@gmail.com>
2021-11-29 19:16:24 +0545

    Add files via upload

--- Problem 4.php
+++ Problem 4.php
@@ -1,5 +1,36 @@
 <?php
-	echo base_convert(24, 10, 8)."</br>";
-	echo base_convert(8,10,2);
-	//It take three arguments i.e number, frombase and tobase.
+	function number_convert($n,$from,$to){
+		if($from == 'decimal'){
+			switch($to){
+				case 'binary':
+					$binaryNum;
+		    		$i = 0;
+		    		while ($n > 0)
+		    		{
+		        		$binaryNum[$i] = $n % 2;
+		        		$n = (int)($n / 2);
+		        		$i++;
+		    		}
+		    		for ($j = $i - 1; $j >= 0; $j--){
+		        		echo $binaryNum[$j];
+		    		}
+		    		echo "</br>";
+
+		        case 'octal':
+					$octalNum;
+		    		$i = 0;
+		    		while ($n > 0)
+		    		{
+		        		$octalNum[$i] = $n % 8;
+		        		$n = (int)($n / 8);
+		        		$i++;
+		    		}
+		    		for ($j = $i - 1; $j >= 0; $j--){
+		        		echo $octalNum[$j];
+		    		}
+			}
+		}
+	}
+ number_convert(42,'decimal','binary');
+ number_convert(42,'decimal','octal');
 ?>
